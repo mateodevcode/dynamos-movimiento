4048d1bee18da25244ef4617920e8ca7f2355ac2
Rey <reydavidquiroz@gmail.com>
2024-05-02 17:41:41 -0500

    Maquetacion, responsive y dark mood Dynamospage

--- src/components/SomosDynamos.jsx
+++ src/components/SomosDynamos.jsx
@@ -1,27 +1,52 @@
-import logoDynamos from '../img/logo-dynamo.png'
+import logoDynamos from "../img/logo-dynamo.png";
 
 function SomosDynamos() {
-
   return (
-    <div className={`flex w-full items-center justify-center`}>
-      <div className="flex flex-row md:mt-32 sm:mt-20">
-        <div className="flex flex-col justify-center items-center w-5/12">
-          <img src={logoDynamos} alt="Logo de dynamos" className='w-64'/>
-          <img src={logoDynamos} alt="Logo de dynamos" className='opacity-10 w-64'/>
+    <div
+      className={`flex flex-col w-full items-center dark:bg-gray-900`}
+    >
+      {/* Recueadro1 */}
+      <div className="flex md:flex-row sm:flex-col justify-around items-center md:mt-20 md:mb-10 sm:mt-20 sm:w-auto">
+        <div className="flex flex-col justify-center items-center md:w-5/12 md:pr-20 sm:mb-4 md:mb-0">
+          <img
+            src={logoDynamos}
+            alt="Logo de dynamos"
+            className=" md:w-48 sm:w-32 "
+          />
         </div>
         <div className={`flex flex-col w-5/12 justify-center items-center`}>
-          <h3 className='text-6xl mb-24 text-left text-yellow-600'>SIGNIFICADO DE LA PALABRA </h3>
-          <p className='text-xl italic'>
-            En griego la raíz de la palabra Dunamis es dynamus, que significa,
-            el poder explosivo del Espíritu Santo con una connotación de
-            dinamita y dinámica.
+          <h3 className="text-6xl mb-10 sm:text-center text-yellow-600">MISIÓN </h3>
+          <p className="text-xl italic sm:text-center md:text-left md:w-auto sm:w-72 text-gray-600 dark:text-gray-300 sm:mx-0 md:mx-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eum
+            ducimus eaque, delectus adipisci obcaecati doloremque. Quis harum
+            pariatur laudantium sed, vero nisi tempora modi quasi necessitatibus
+            optio facilis magnam..
           </p>
         </div>
         <div></div>
         <div></div>
       </div>
+      {/* Recueadro2 */}
+      <div className="flex md:flex-row sm:flex-col justify-around items-center md:ml-32 md:mt-0 sm:mt-20">
+        <div className={`flex flex-col w-5/12 justify-center items-center sm:mb-4 md:mb-0`}>
+          <h3 className="text-6xl mb-10 text-left  text-yellow-600">VISIÓN </h3>
+          <p className="text-xl text-gray-600 dark:text-gray-300 italic sm:text-center md:text-left md:w-auto sm:w-72">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eum
+            ducimus eaque, delectus adipisci obcaecati doloremque. Quis harum
+            pariatur laudantium sed, vero nisi tempora modi quasi necessitatibus
+            optio facilis magnam..
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center sm:w-auto md:w-5/12 ">
+          <img
+            src={logoDynamos}
+            alt="Logo de dynamos"
+            className="opacity-10 md:w-48 sm:w-32"
+          />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default SomosDynamos
+export default SomosDynamos;
