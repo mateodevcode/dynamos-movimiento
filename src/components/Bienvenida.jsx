89ec81bfb07d68a55a5a7e65dfe8495ce3e5d106
Mateo Lizcano <mateodevcode@gmail.com>
2024-05-10 19:22:35 +0200

    Se actualizo el color del titulo y parrafo de la pagina de bienvenida

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -9,8 +9,8 @@ function Bienvenida() {
     <div className={`flex w-full justify-center items-center dark:bg-gray-900 lg:pt-40 md:pt-30 sm:pt-12 ${darkMode ? "fondo-estandar-oscuro" : "fondo-estandar-claro"}`}>
       <div className="w-10/12 flex md:flex-row md:pb-24 sm:flex-col sm:justify-center sm:items-center">
         <div className="flex flex-col md:w-11/12 md:text-left md:m-10 md:mt-32 sm:mt-12">
-          <h2 className="md:text-6xl md:text-left font-bold md:mb-10 sm:text-2xl sm:text-center sm:mb-10 text-white">
-            Bienvenidos al Movimiento <strong className='text-black'>Dynamos</strong>
+          <h2 className="md:text-6xl md:text-left font-bold md:mb-10 sm:text-2xl sm:text-center sm:mb-10 text-white sm:text-black dark:sm:text-white">
+            Bienvenidos al Movimiento <strong className='text-black sm:text-white'>Dynamos</strong>
           </h2>
           <p className="md:flex md:text-xl text-gray-200 sm:mb-5 sm:hidden">
             Aquí podrán ver lo que hacemos como iglesia, cómo servimos a Dios y
@@ -27,7 +27,7 @@ function Bienvenida() {
             />
           </div>
         </div>
-        <p className="sm:flex md:hidden text-sm text-justify w-10/12 sm:mb-20 text-gray-600 dark:text-gray-300">
+        <p className="sm:flex md:hidden text-sm text-justify w-10/12 sm:mb-20 text-white">
           Aquí podrán ver lo que hacemos como iglesia, cómo servimos a Dios y a
           nuestro prójimo, y cómo pueden unirse a nosotros en este camino de
           fe..
